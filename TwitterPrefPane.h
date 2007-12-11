2b6eff05d998b8cc88bd071e7016996bedb9ccfe
mootoh <mootoh@d0d07461-0603-4401-acd4-de1884942a52>
2007-12-11 16:58:00 +0000

    add nib to show up prefpane interface.
    git-svn-id: http://svn.coderepos.org/share/platform/quicksilver/TwitterPlugin@3066 d0d07461-0603-4401-acd4-de1884942a52

---
+++ TwitterPrefPane.h
@@ -0,0 +1,14 @@
+//
+//  TwitterPrefPane.h
+//  TwitterPlugin
+//
+//  Created by Motohiro Takayama on 12/12/07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import <QSInterface/QSPreferencePane.h>
+
+@interface TwitterPrefPane : QSPreferencePane {
+}
+
+@end
---
+++ TwitterPrefPane.m
@@ -0,0 +1,18 @@
+//
+//  TwitterPrefPane.m
+//  TwitterPlugin
+//
+//  Created by Motohiro Takayama on 12/12/07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import "TwitterPrefPane.h"
+
+
+@implementation TwitterPrefPane
+
+-(NSString *)mainNibName {
+	return @"TwitterPrefPane";
+}
+
+@end
