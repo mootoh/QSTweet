52fd1422e85e663d4f78eda31f0e2d2976ae027f
mootoh <mootoh@d0d07461-0603-4401-acd4-de1884942a52>
2007-12-11 17:04:44 +0000

    platform/quicksilver/TwitterPlugin: add HTTP POST skelton.
    git-svn-id: http://svn.coderepos.org/share/platform/quicksilver/TwitterPlugin@3068 d0d07461-0603-4401-acd4-de1884942a52

--- TwitterPluginAction.h
+++ TwitterPluginAction.h
@@ -14,6 +14,7 @@
 
 @interface TwitterPluginAction : QSActionProvider
 {
+  NSData *receivedData;
 }
 @end
 
--- TwitterPluginAction.m
+++ TwitterPluginAction.m
@@ -11,6 +11,55 @@
 @implementation TwitterPluginAction
 
 - (QSObject *)performActionOnObject:(QSObject *)dObject{
-	return nil;
+  QSObject *result;
+  NSString *dWithHello;
+
+  dWithHello = [NSString stringWithFormat:@"%@ hello",[dObject stringValue]];
+  result = [QSObject objectWithString:dWithHello];
+
+
+  NSString *content = @"post from Cocoa NSURLRequest.";
+  NSURL *url = [NSURL URLWithString:@"http://user:pass@twitter.com/statuses/update.json"];
+  NSMutableURLRequest *urlRequest = [[NSMutableURLRequest alloc] initWithURL:url];
+  [urlRequest setHTTPMethod:@"POST"];
+  [urlRequest setHTTPBody:[content dataUsingEncoding:NSASCIIStringEncoding]];
+
+  NSLog(@"before");
+
+/*
+  // create the request
+  NSURLRequest *theRequest=[NSURLRequest
+    requestWithURL:[NSURL URLWithString:@"http://www.apple.com/"]
+    cachePolicy:NSURLRequestUseProtocolCachePolicy
+    timeoutInterval:60.0];
+  // create the connection with the request
+  // and start loading the data
+*/
+  NSURLConnection *theConnection = [NSURLConnection
+    connectionWithRequest:urlRequest
+    delegate:self];
+  if (theConnection) {
+    // Create the NSMutableData that will hold
+    // the received data
+    // receivedData is declared as a method instance elsewhere
+    receivedData = [[NSMutableData data] retain];
+    NSLog(@"connected !");
+  } else {
+    NSLog(@"not connected correctly.");
+    // inform the user that the download could not be made
+  }
+
+  return result;
 }
+
+- (void) connection : (NSURLConnection *) connection
+         didReceiveResponse : (NSURLResponse *) response {
+  NSLog(@"didReceiveResponse 1");
+
+  NSDictionary *dicHead = [(NSHTTPURLResponse *)response allHeaderFields];
+  NSLog([dicHead objectForKey:@"Last-Modified"]);
+  //NSLog(@"didReceiveResponse 2");
+  //abort();
+}
+
 @end
